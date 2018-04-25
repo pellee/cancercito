7577b7f62f9b08337dd05ae2248fd365d85ab651
Pelle <joaquinpellegrino@gmail.com>
2018-04-25 16:26:11 -0300

    Cancercito

---
+++ Alumnos/Alumno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumnos
+{
+    class Alumno
+    {
+        public int Id { get; set; }
+        public string Apellido { get; set; }
+        public double Promedio { get; set; }
+
+
+        public void ListarAlumnos(Alumno[] alumnos)
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+                Console.WriteLine(alumnos[i].Id + "  " + alumnos[i].Apellido + "  " + alumnos[i].Promedio);
+        }
+
+        public int BuscarId(int id, Alumno[] alumnos)
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                if (id == alumnos[i].Id)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Alumnos/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumnos
+{
+    class Program
+    {
+        static Alumno CrearAlumno()
+        {
+            var aux = new Alumno();
+
+            Console.WriteLine("Ingrese apellido del alumno: ");
+            aux.Apellido = Console.ReadLine();
+            do {
+                Console.WriteLine("Ingrese promedio alumno: ");
+                aux.Promedio = double.Parse(Console.ReadLine());
+            } while (aux.Promedio < 1 || aux.Promedio > 10);
+
+            aux.Id = 1;
+
+            return aux;
+        }
+
+        static void Menu()
+        {
+
+            Alumno[] alumnos = new Alumno[3];
+            var aux = new Alumno();
+            int id;
+            char opc = ' ';
+
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                alumnos[i] = CrearAlumno();
+                alumnos[i].Id += i;
+            }
+
+            do
+            {
+                aux.ListarAlumnos(alumnos);
+
+                Console.WriteLine("escriba numero del alumno que le quiere cambiar el promedio");
+                id = int.Parse(Console.ReadLine());
+
+                id = aux.BuscarId(id, alumnos);
+
+                if (id != -1)
+                {
+                    do
+                    {
+                        Console.WriteLine("Ingrese promedio alumno: ");
+                        alumnos[id].Promedio = double.Parse(Console.ReadLine());
+                    } while (alumnos[id].Promedio < 1 || alumnos[id].Promedio > 10);
+                }
+                else
+                    Console.WriteLine("No se encontro el id");
+
+                Console.WriteLine("Para salir presione N");
+                opc = char.Parse(Console.ReadLine());
+
+                opc = char.ToUpper(opc);
+            } while (opc != 'N');
+        }
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
